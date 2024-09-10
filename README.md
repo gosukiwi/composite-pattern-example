d9d8ef1524005a86cc8961d6ce06b99d52313c85
Federico Ramirez <federico_r@beezwax.net>
2024-09-10 13:10:02 -0700

    Pattern

---
+++ README.md
@@ -0,0 +1,8 @@
+# Composite Pattern
+
+A minimalistic example. You can run it with:
+
+    $ npm i
+    $ npm run test
+
+See `./tests/composite.test.ts` for the implementation.
--- tests/composite.test.ts
+++ tests/composite.test.ts
@@ -1,5 +1,55 @@
 import { expect, test } from "vitest";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(1).toBe(1);
+// This is the interface implemented by both single objects and composite
+// objects.
+interface Priceable {
+  getPrice(): number;
+}
+
+// This is a single product, the implementation is straight forward.
+class Product implements Priceable {
+  name: string;
+  price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+}
+
+// This is a composite, because one box has many products. But from the outside
+// world, we don't care. We simply use `getPrice()`.
+class Box implements Priceable {
+  products: Product[];
+
+  constructor(products: Product[]) {
+    this.products = products;
+  }
+
+  getPrice() {
+    return this.products.reduce(
+      (accu, product) => accu + product.getPrice(),
+      0,
+    );
+  }
+}
+
+test("it can be used indistinctively", () => {
+  const phone = new Product("iPhone", 1000);
+  const tablet = new Product("iPad", 600);
+  const box = new Box([phone, tablet]);
+
+  // We can call `getPrice()` on a single product, or a box of products. We
+  // don't really care if our object is a composite or not.
+  //
+  // The whole point of the pattern is being able to treat composites (Boxes)
+  // and non-composites (Products) in the same way.
+  //
+  expect(phone.getPrice()).toEqual(1000);
+  expect(tablet.getPrice()).toEqual(600);
+  expect(box.getPrice()).toEqual(1600);
 });
